c3dc8809936f0f906f8b3b28ba13fe2ffff838a9
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-06-26 18:45:17 -0700

    Allow hotreload for the server.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(3001, () => {
+  console.log("listening on *:3001");
 });

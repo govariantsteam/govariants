0736038befd6a80224fdd3985c93f5f002402314
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-07-05 23:15:40 -0700

    Integrate socket.io.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import io from "socket.io-client";
+
+const socket = io(`http://${window.location.hostname}:3000`);
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState<string | null>(null);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    socket.on("pong", () => {
+      setLastPong(new Date().toISOString());
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("pong");
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit("ping");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +50,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <p>Connected: {"" + isConnected}</p>
+        <p>{`Last pong: ${lastPong || "-"}`}</p>
+        <button onClick={sendPing}>Send ping</button>
+      </div>
     </div>
   );
 }
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,13 +1,28 @@
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:8080",
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-server.listen(3001, () => {
-  console.log("listening on *:3001");
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("ping", function (data) {
+    io.emit("pong", data);
+    console.log("ping");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });

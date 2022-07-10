bd783917a35be73c2fa29bc1383516622aeb46c6
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-07-10 09:26:58 -0700

    Serve React from express so we don't have to set up two servers.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import { getGame, getGames, createGame, playMove } from "./games";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path";
 
 const LOCAL_ORIGIN = "http://localhost:3000";
 
@@ -21,10 +22,6 @@ const io = new Server(server, {
   cors: {
     origin: LOCAL_ORIGIN,
   },
-});
-
-app.get("/", (_req, res) => {
-  res.send("<h1>Hello world</h1>");
 });
 
 app.get("/games/:gameId", (req, res) => {
@@ -62,6 +59,19 @@ io.on("connection", (socket) => {
     console.log("ping");
   });
 });
+
+// If production, serve the React repo!
+const isProd = process.env.NODE_ENV === "production";
+if (isProd) {
+  // Compute the build path and index.html path
+  const build_path = path.join(__dirname, "../../../packages/client/build");
+  const indexHtml = path.join(build_path, "index.html");
+
+  // Setup build path as a static assets path
+  app.use(express.static(build_path));
+  // Serve index.html on unmatched routes
+  app.get("*", (_req, res) => res.sendFile(indexHtml));
+}
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {

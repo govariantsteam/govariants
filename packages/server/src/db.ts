b22796f266c26e0f47014acb65d31a6c947122e6
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-07-26 08:58:40 -0700

    Add a db.
    This adds mongo as a dependency.  Another notable change is that IDs are now the
    default mongo IDs instead of auto-incrementing numbers.

---
+++ packages/server/src/db.ts
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+
+let client: MongoClient = undefined;
+
+export async function connectToDb() {
+  const ATLAS_URI = process.env.ATLAS_URI || "mongodb://127.0.0.1:27017";
+
+  // initialize MongoDB
+  client = new MongoClient(ATLAS_URI);
+  try {
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("govariants").command({ ping: 1 });
+    console.log("Connected successfully to database");
+  } catch {
+    console.log("Failed to connect to database");
+  }
+}
+
+export function getDb() {
+  if (!client) {
+    throw Error("Could not connect to database.");
+  }
+
+  return client;
+}

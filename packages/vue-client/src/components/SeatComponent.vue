28992f0e91e636909773af44d5d0a76de43a343a
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-08-17 08:14:26 -0700

    Created the SeatComponent.

---
+++ packages/vue-client/src/components/SeatComponent.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+interface UserT {
+  username?: string;
+  id: string;
+}
+</script>
+
+<script setup lang="ts">
+import type { PropType } from "vue";
+
+function takeSeat() {
+  // TODO: hit the server with this
+}
+
+function leaveSeat() {
+  // TODO: hit the server
+}
+
+defineProps({
+  user_id: { required: true, type: String },
+  occupant: Object as PropType<UserT>,
+  player_n: { required: true, type: Number },
+});
+</script>
+
+<template>
+  <div class="seat">
+    <p class="seat-number">{{ player_n }}</p>
+    <div v-if="occupant === undefined">
+      <button @click="takeSeat">Take Seat</button>
+    </div>
+    <div v-else>
+      <p>{{ occupant.username ?? "guest" }}</p>
+      <button v-if="occupant.id === user_id" @click="leaveSeat">
+        Leave Seat
+      </button>
+    </div>
+  </div>
+</template>
+
+<style>
+.seat {
+  border-radius: 10px;
+  border: 2px solid gray;
+  display: inline-block;
+  width: 200px;
+  padding: 10px;
+  margin: 2px;
+}
+.seat div {
+  display: inline-block;
+}
+.seat-number {
+  font-size: x-large;
+  display: inline-block;
+  margin-right: 10px;
+}
+</style>
--- packages/vue-client/src/views/GameView.vue
+++ packages/vue-client/src/views/GameView.vue
@@ -2,6 +2,7 @@
 import type { GameResponse } from "@ogfcommunity/variants-shared";
 import Baduk from "@/components/boards/BadukBoard.vue";
 import * as requests from "../requests";
+import SeatComponent from "@/components/SeatComponent.vue";
 
 const props = defineProps({
   gameId: String,
@@ -11,6 +12,16 @@ const gameResponse = (await requests.get(
   `/games/${props.gameId}`
 )) as GameResponse;
 const variantGameView = gameResponse.variant === "baduk" ? Baduk : null;
+
+const dummy_players = [
+  { username: "benjito", id: "asdf" },
+  undefined,
+  { username: "not_benjito", id: "asdfg" },
+  // guest
+  { id: "1234" },
+  // current guest
+  { id: "asdf" },
+];
 </script>
 
 <template>
@@ -19,6 +30,12 @@ const variantGameView = gameResponse.variant === "baduk" ? Baduk : null;
     v-bind:is="variantGameView"
     v-bind:gameResponse="gameResponse"
   />
+  <p style="color: red">
+    WARNING: These seats don't work yet. Just testing the SeatComponent...
+  </p>
+  <div v-for="(player, idx) in dummy_players" :key="idx">
+    <SeatComponent user_id="asdf" :occupant="player" :player_n="idx" />
+  </div>
   <pre>
     <span>from /games/{{gameResponse.id}}</span>
     {{JSON.stringify(gameResponse, null, 2)}}

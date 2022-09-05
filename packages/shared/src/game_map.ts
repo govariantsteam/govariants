616c5083dcee312f86be76674e229de47294b1e1
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-09-05 08:49:45 -0700

    Add better types to game_map and board_map.

--- packages/shared/src/game_map.ts
+++ packages/shared/src/game_map.ts
@@ -2,9 +2,9 @@ import { Baduk } from "./baduk";
 import { AbstractGame } from "./abstract_game";
 import { Phantom } from "./phantom";
 
-// Is there any way we can get away from `any` here?
-// I think only the infra needs to touch these functions, but still!
-export const game_map: { [variant: string]: any } = {
+export const game_map: {
+  [variant: string]: new (config: any) => AbstractGame;
+} = {
   baduk: Baduk,
   phantom: Phantom,
 };
--- packages/vue-client/src/board_map.ts
+++ packages/vue-client/src/board_map.ts
@@ -1,9 +1,8 @@
 import Baduk from "@/components/boards/BadukBoard.vue";
+import type { Component } from "vue";
 
 export const board_map: {
-  // TODO: add types - I have no idea how this works for Vue components.
-  // For React analog, see https://github.com/benjaminpjones/govariants/blob/06e2bb81b42c1a4ec4d2bcb85be621112dbc4b90/packages/client/src/view_types.ts
-  [variant: string]: any;
+  [variant: string]: Component;
 } = {
   baduk: Baduk,
   phantom: Baduk,

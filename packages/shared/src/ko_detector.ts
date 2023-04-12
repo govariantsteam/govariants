b74c81c298af44e09a3a493a5aea1242b8f63973
Benjamin Jones <benjaminpaikjones@gmail.com>
2023-04-11 23:27:51 -0700

    Better error message

--- packages/shared/src/ko_detector.ts
+++ packages/shared/src/ko_detector.ts
@@ -3,7 +3,7 @@ export class SuperKoDetector<T> {
 
   push(state: T): void {
     const state_str = JSON.stringify(state);
-    if (this.seen.has(state_str)) throw new KoError();
+    if (this.seen.has(state_str)) throw new KoError("state repeated");
     this.seen.add(state_str);
   }
 }

725e52b5435853683ada58153edb4d560097d509
Benjamin Jones <benjaminpaikjones@gmail.com>
2023-04-16 14:38:51 -0700

    Don't double test tests.
    jest was running the tests in dist/ as well as the ts files directly.

--- packages/shared/jest.config.ts
+++ packages/shared/jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@jest/types";
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
+  roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },

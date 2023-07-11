a9362775ca9c773b2b8f761341e9c1e594511448
Jon Ko <jon.ko@gmx.net>
2023-07-11 22:25:29 +0200

    Fix fractional board size in game list

--- packages/vue-client/src/components/boards/FractionalBoard.vue
+++ packages/vue-client/src/components/boards/FractionalBoard.vue
@@ -19,81 +19,103 @@ function intersectionClicked(identifier: number) {
   emit("move", identifier);
 }
 
+const boardRect = computed(
+  (): { x: number; y: number; width: number; height: number } => {
+    const xPositions = props.gamestate.intersections.map((i) => i.position.X);
+    const yPositions = props.gamestate.intersections.map((i) => i.position.Y);
+    const xMin = Math.min(...xPositions);
+    const xMax = Math.max(...xPositions);
+    const yMin = Math.min(...yPositions);
+    const yMax = Math.max(...yPositions);
+    return {
+      x: xMin - 0.5,
+      y: yMin - 0.5,
+      width: xMax - xMin + 1,
+      height: yMax - yMin + 1,
+    };
+  }
+);
+
 const viewBox = computed(() => {
-  const xPositions = props.gamestate.intersections.map((i) => i.position.X);
-  const yPositions = props.gamestate.intersections.map((i) => i.position.Y);
-  return `${Math.min(...xPositions) - 1} ${Math.min(...yPositions) - 1} ${
-    Math.max(...xPositions) + 2
-  } ${Math.max(...yPositions) + 2}`;
+  // viewBox is slightly larger than board
+  const x = boardRect.value.x - 0.5;
+  const y = boardRect.value.y - 0.5;
+  const width = boardRect.value.width + 1;
+  const height = boardRect.value.height + 1;
+  return `${x} ${y} ${width} ${height}`;
 });
 
 const stagedMove = computed(() => props.gamestate.stagedMove);
 </script>
 
 <template>
-  <div>
-    <h3 style="color: red">Experimental</h3>
-    <svg
-      class="board"
-      xmlns="http://www.w3.org/2000/svg"
-      width="600px"
-      height="600px"
-      :viewBox="viewBox"
-    >
-      <g class="stones">
-        <g
-          v-for="intersection in props.gamestate.intersections"
-          :key="intersection.id"
-        >
-          <line
-            v-for="neighbour in intersection.neighbours.filter(
-              (n) => n.id < intersection.id
-            )"
-            :key="neighbour.id"
-            class="grid"
-            :x1="intersection.position.X"
-            :x2="neighbour.position.X"
-            :y1="intersection.position.Y"
-            :y2="neighbour.position.Y"
-          />
-        </g>
+  <svg
+    class="board"
+    xmlns="http://www.w3.org/2000/svg"
+    width="100%"
+    height="100%"
+    :viewBox="viewBox"
+  >
+    <rect
+      class="background"
+      :x="boardRect.x"
+      :y="boardRect.y"
+      :width="boardRect.width"
+      :height="boardRect.height"
+    />
+    <g class="lines">
+      <g
+        v-for="intersection in props.gamestate.intersections"
+        :key="intersection.id"
+      >
+        <line
+          v-for="neighbour in intersection.neighbours.filter(
+            (n) => n.id < intersection.id
+          )"
+          :key="neighbour.id"
+          class="grid"
+          :x1="intersection.position.X"
+          :x2="neighbour.position.X"
+          :y1="intersection.position.Y"
+          :y2="neighbour.position.Y"
+        />
       </g>
-      <g class="stones">
-        <g
-          v-for="intersection in props.gamestate.intersections"
-          :key="intersection.id"
-        >
-          <TaegeukStone
-            v-if="intersection.stone"
-            :colors="[...intersection.stone.colors]"
-            :cx="intersection.position.X"
-            :cy="intersection.position.Y"
-            :r="0.4"
-          />
-          <circle
-            v-else
-            class="click-placeholder"
-            v-on:click="intersectionClicked(intersection.id)"
-            v-bind:cx="intersection.position.X"
-            v-bind:cy="intersection.position.Y"
-            r="0.4"
-          />
-        </g>
+    </g>
+    <g class="stones">
+      <g
+        v-for="intersection in props.gamestate.intersections"
+        :key="intersection.id"
+      >
+        <TaegeukStone
+          v-if="intersection.stone"
+          :colors="[...intersection.stone.colors]"
+          :cx="intersection.position.X"
+          :cy="intersection.position.Y"
+          :r="0.4"
+        />
+        <circle
+          v-else
+          class="click-placeholder"
+          v-on:click="intersectionClicked(intersection.id)"
+          v-bind:cx="intersection.position.X"
+          v-bind:cy="intersection.position.Y"
+          r="0.4"
+        />
       </g>
-      <TaegeukStone
-        v-if="stagedMove"
-        :colors="stagedMove.colors"
-        :cx="stagedMove.intersection.position.X"
-        :cy="stagedMove.intersection.position.Y"
-        :r="0.4"
-      />
-    </svg>
-  </div>
+    </g>
+    <TaegeukStone
+      v-if="stagedMove"
+      :colors="stagedMove.colors"
+      :cx="stagedMove.intersection.position.X"
+      :cy="stagedMove.intersection.position.Y"
+      :r="0.4"
+    />
+  </svg>
 </template>
 
 <style scoped>
-svg.board {
-  background-color: #dcb35c;
+.board .background {
+  fill: #dcb35c;
 }
 
 line {

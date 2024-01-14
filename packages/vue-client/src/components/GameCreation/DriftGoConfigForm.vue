993cb816182d3faee683170185b81c509178dd27
merowin <martin.unger@planet-earth.at>
2024-01-14 05:12:10 +0100

    add config form

---
+++ packages/vue-client/src/components/GameCreation/DriftGoConfigForm.vue
@@ -0,0 +1,34 @@
+import { DriftGoConfig } from '../../../../shared/dist/variants/drift';
+<script setup lang="ts">
+const props = defineProps<{ initialConfig: DriftGoConfig }>();
+const config = { ...props.initialConfig } as DriftGoConfig;
+
+const emit = defineEmits<{
+  (e: "configChanged", config: DriftGoConfig): void;
+}>();
+
+function emitConfigChange() {
+  emit("configChanged", config);
+}
+</script>
+
+<template>
+  <form @change="emitConfigChange" class="config-form-column">
+    <label>Width</label>
+    <input type="number" min="1" v-model="config.width" />
+    <label>Height</label>
+    <input type="number" min="1" v-model="config.height" />
+    <label>Komi</label>
+    <input type="number" step="0.5" v-model="config.komi" />
+    <label>X-Shift</label>
+    <input type="number" step="1" v-model="config.yShift" />
+    <label>Y-Shift</label>
+    <input type="number" step="1" v-model="config.yShift" />
+  </form>
+</template>
+
+<style>
+input {
+  width: fit-content;
+}
+</style>

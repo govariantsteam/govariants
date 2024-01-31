227ae140c14261b15c00a8c4f6189f9fdf57e6f0
Eva U. <87281301+evizam@users.noreply.github.com>
2024-01-31 23:54:08 +0100

    Add next to play indicators to seats
    Make it easier to see which players' turn it currently is by
    highlighting their seat components. Also display the list of players
    to move in the game view..

--- packages/vue-client/src/components/SeatComponent.vue
+++ packages/vue-client/src/components/SeatComponent.vue
@@ -11,12 +11,15 @@ defineProps<{
   player_n: number;
   selected?: number;
   time_control: IPerPlayerTimeControlBase | null;
+  is_players_turn: boolean;
 }>();
 </script>
 
 <template>
   <div
-    v-bind:class="`seat ${selected === player_n ? 'selected' : ''}`"
+    v-bind:class="`seat ${selected === player_n ? 'selected' : ''} ${
+      is_players_turn ? 'to-move' : ''
+    }`"
     @click="$emit('select')"
   >
     <p class="seat-number">{{ player_n }}</p>
@@ -25,7 +28,9 @@ defineProps<{
       <button v-if="user_id" @click.stop="$emit('sit')">Take Seat</button>
     </div>
     <div v-else>
-      <p>{{ occupant.username ?? `guest (...${occupant.id.slice(-6)})` }}</p>
+      <p class="seat-username">
+        {{ occupant.username ?? `guest (...${occupant.id.slice(-6)})` }}
+      </p>
       <button v-if="occupant.id === user_id" @click.stop="$emit('leave')">
         Leave Seat
       </button>
@@ -41,10 +46,10 @@ defineProps<{
   display: inline-block;
   width: 200px;
   padding: 10px;
-  margin: 2px;
+  margin: 5px 2px;
 }
 .seat.selected {
-  border-color: deepskyblue;
+  border-color: var(--color-selected-seat);
 }
 .seat div {
   display: inline-block;
@@ -53,5 +58,20 @@ defineProps<{
   font-size: x-large;
   display: inline-block;
   margin-right: 10px;
+}
+.seat.to-move {
+  box-shadow: 0px 0px var(--to-move-shadow-width) var(--color-shadow);
+}
+
+.seat.to-move.selected {
+  box-shadow: 0px 0px var(--to-move-shadow-width) var(--color-selected-seat);
+}
+
+.seat.to-move .seat-number {
+  font-weight: 600;
+}
+
+.seat.to-move .seat-username {
+  font-weight: bold;
 }
 </style>

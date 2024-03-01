9e2758a13c2c82d288f53f26ba2ead7178469957
Eva U. <87281301+evizam@users.noreply.github.com>
2024-03-01 02:12:24 +0100

    Remove unused css

--- packages/vue-client/src/App.vue
+++ packages/vue-client/src/App.vue
@@ -59,34 +59,6 @@ nav {
   display: none;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-
 @media (max-width: 768px) {
   .navContent {
     flex-direction: column;

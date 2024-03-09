2026c2196d66026a5b8bbf011062ad2937912a64
merowin <martin.unger@planet-earth.at>
2024-03-09 13:27:57 +0100

    simplify

--- packages/shared/src/variants/badukWithAbstractBoard/abstractBoard/SierpinskyBoard.ts
+++ packages/shared/src/variants/badukWithAbstractBoard/abstractBoard/SierpinskyBoard.ts
@@ -4,25 +4,23 @@ import { Intersection } from "./intersection";
 export function createSierpinskyBoard(depth: number): Intersection[] {
   const sideLength = Math.pow(2, depth + 1);
 
-  const top = new Vector2D(0, 0);
-  const topCorner = new Intersection(top);
+  const topCorner = new Intersection(new Vector2D(0, 0));
+  const leftCorner = new Intersection(
+    new Vector2D(
+      Math.cos((2 * Math.PI) / 3),
+      Math.sin((2 * Math.PI) / 3),
+    ).Multiply(sideLength),
+  );
+  const rightCorner = new Intersection(
+    new Vector2D(Math.cos(Math.PI / 3), Math.sin(Math.PI / 3)).Multiply(
+      sideLength,
+    ),
+  );
 
-  const left = new Vector2D(
-    Math.cos((2 * Math.PI) / 3),
-    Math.sin((2 * Math.PI) / 3),
-  ).Multiply(sideLength);
-  const leftCorner = new Intersection(left);
-
-  const right = new Vector2D(
-    Math.cos(Math.PI / 3),
-    Math.sin(Math.PI / 3),
-  ).Multiply(sideLength);
-  const rightCorner = new Intersection(right);
-
-  const sierpinskyTriangle = new SierpinskyTriangle([top, left, right], depth);
-  sierpinskyTriangle.connectCorner(topCorner, 0);
-  sierpinskyTriangle.connectCorner(leftCorner, 1);
-  sierpinskyTriangle.connectCorner(rightCorner, 2);
+  const sierpinskyTriangle = new SierpinskyTriangle(
+    [topCorner, leftCorner, rightCorner],
+    depth,
+  );
 
   return [
     topCorner,
@@ -40,43 +38,38 @@ class SierpinskyTriangle {
 
   private middleIntersections: Intersection[];
 
-  constructor(corners: [Vector2D, Vector2D, Vector2D], depth: number) {
-    const middles = corners.map((corner, index) =>
-      corner.Add(corners[(index + 1) % 3]).Multiply(0.5),
-    );
-    this.middleIntersections = middles.map(
-      (vector) => new Intersection(vector),
+  constructor(
+    corners: [Intersection, Intersection, Intersection],
+    depth: number,
+  ) {
+    this.middleIntersections = corners.map(
+      (corner, index) =>
+        new Intersection(
+          corner.Position.Add(corners[(index + 1) % 3].Position).Multiply(0.5),
+        ),
     );
 
     if (depth > 0) {
-      this.subTriangles = corners.map((corner, index) => {
-        const triangle = new SierpinskyTriangle(
-          [corner, middles[index], middles[(index + 2) % 3]],
-          depth - 1,
-        );
-
-        triangle.connectCorner(this.middleIntersections[index], 1);
-        triangle.connectCorner(this.middleIntersections[(index + 2) % 3], 2);
-
-        return triangle;
-      });
+      this.subTriangles = corners.map(
+        (corner, index) =>
+          new SierpinskyTriangle(
+            [
+              corner,
+              this.middleIntersections[index],
+              this.middleIntersections[(index + 2) % 3],
+            ],
+            depth - 1,
+          ),
+      );
     } else {
       this.subTriangles = null;
 
-      this.middleIntersections.forEach((intersection, index) =>
-        intersection.ConnectTo(this.middleIntersections[(index + 1) % 3], true),
-      );
+      this.middleIntersections.forEach((intersection, index) => {
+        intersection.ConnectTo(this.middleIntersections[(index + 1) % 3], true);
+        intersection.ConnectTo(corners[index], true);
+        intersection.ConnectTo(corners[(index + 1) % 3], true);
+      });
     }
-  }
-
-  connectCorner(corner: Intersection, index: number) {
-    if (this.subTriangles) {
-      this.subTriangles[index].connectCorner(corner, 0);
-      return;
-    }
-
-    this.middleIntersections[index].ConnectTo(corner, true);
-    this.middleIntersections[(index + 2) % 3].ConnectTo(corner, true);
   }
 
   flatten(): Intersection[] {

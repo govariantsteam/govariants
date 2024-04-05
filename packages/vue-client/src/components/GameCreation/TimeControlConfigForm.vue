fcba839ee4f36768420092d763d60b8018b63731
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2024-04-04 22:29:36 -0700

    Add new time controls to the UI.

--- packages/vue-client/src/components/GameCreation/TimeControlConfigForm.vue
+++ packages/vue-client/src/components/GameCreation/TimeControlConfigForm.vue
@@ -1,6 +1,16 @@
+<script lang="ts">
+import { TimeControlType } from "@ogfcommunity/variants-shared/src/time_control";
+function shouldShowPeriodTime(type: TimeControlType) {
+  return type == TimeControlType.ByoYomi || type === TimeControlType.Canadian;
+}
+</script>
+
 <script setup lang="ts">
+import type {
+  IByoYomiConfig,
+  ICanadianTimeConfig,
+} from "@ogfcommunity/variants-shared";
 import {
-  TimeControlType,
   type ITimeControlConfig,
   type IFischerConfig,
 } from "@ogfcommunity/variants-shared/src/time_control";
@@ -8,6 +18,8 @@ import { ref } from "vue";
 
 const typeRef = ref(null);
 const mainTimeS = ref(300);
+const periodTimeS = ref(30);
+const numPeriods = ref(5);
 const fischerIncrementS = ref(20);
 const fischerMaxS = ref(600);
 const fischerCapped = ref(true);
@@ -21,9 +33,10 @@ function emitConfigChange() {
 
   if (typeRef.value !== null) {
     switch (typeRef.value) {
-      case TimeControlType.Absolute: {
+      case TimeControlType.Absolute:
+      case TimeControlType.Simple: {
         emitConfig = {
-          type: TimeControlType.Absolute,
+          type: typeRef.value,
           mainTimeMS: mainTimeS.value * 1000,
         };
         break;
@@ -35,6 +48,26 @@ function emitConfigChange() {
           incrementMS: fischerIncrementS.value * 1000,
           maxTimeMS: fischerCapped.value ? fischerMaxS.value * 1000 : null,
         };
+        break;
+      }
+      case TimeControlType.ByoYomi: {
+        const byoConfig: IByoYomiConfig = {
+          type: TimeControlType.ByoYomi,
+          mainTimeMS: mainTimeS.value * 1000,
+          numPeriods: numPeriods.value,
+          periodTimeMS: periodTimeS.value * 1000,
+        };
+        emitConfig = byoConfig;
+        break;
+      }
+      case TimeControlType.Canadian: {
+        const canadianConfig: ICanadianTimeConfig = {
+          type: TimeControlType.Canadian,
+          numPeriods: numPeriods.value,
+          periodTimeMS: periodTimeS.value * 1000,
+          mainTimeMS: mainTimeS.value * 1000,
+        };
+        emitConfig = canadianConfig;
         break;
       }
     }
@@ -51,6 +84,9 @@ function emitConfigChange() {
       <option :value="null">Unlimited Time</option>
       <option :value="TimeControlType.Absolute">Absolute</option>
       <option :value="TimeControlType.Fischer">Fischer</option>
+      <option :value="TimeControlType.ByoYomi">Byo-Yomi</option>
+      <option :value="TimeControlType.Canadian">Canadian Byo-Yomi</option>
+      <option :value="TimeControlType.Simple">Simple</option>
     </select>
     <template v-if="typeRef !== null">
       <label>Main Time (s)</label>
@@ -64,6 +100,12 @@ function emitConfigChange() {
         <input id="cappedToggle" type="checkbox" v-model="fischerCapped" />
       </div>
       <input v-if="fischerCapped" type="number" v-model="fischerMaxS" />
+    </template>
+    <template v-if="typeRef && shouldShowPeriodTime(typeRef)">
+      <label>Number of Periods</label>
+      <input type="number" v-model="numPeriods" />
+      <label>Period time</label>
+      <input type="number" v-model="periodTimeS" />
     </template>
   </form>
 </template>

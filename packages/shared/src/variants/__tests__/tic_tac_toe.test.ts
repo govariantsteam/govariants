13e631d7bae87fe86a98c928236b5eab62706093
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2024-04-21 22:26:00 -0400

    Generate boilerplate for new variant.

---
+++ packages/shared/src/variants/__tests__/tic_tac_toe.test.ts
@@ -0,0 +1,27 @@
+import { TicTacToe } from "../tic_tac_toe";
+
+// These variables just make the boards a little prettier
+const B = 0;
+const _ = null;
+const W = 1;
+
+test("Play a game", () => {
+  const game = new TicTacToe({ width: 4, height: 4 });
+  // Tiny board:
+  // B - - -
+  // - W - -
+  // - - B -
+  // - - - W
+
+  game.playMove(0, "aa");
+  game.playMove(1, "bb");
+  game.playMove(0, "cc");
+  game.playMove(1, "dd");
+
+  expect(game.exportState().board).toEqual([
+    [B, _, _, _],
+    [_, W, _, _],
+    [_, _, B, _],
+    [_, _, _, W],
+  ]);
+});

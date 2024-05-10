637bbc1ee017eb9233cfb9cbc9c666fe70381524
merowin <martin.unger@planet-earth.at>
2024-05-10 05:29:09 +0200

    remove unused code, use group utils in tetris

--- packages/shared/src/variants/baduk_utils.ts
+++ packages/shared/src/variants/baduk_utils.ts
@@ -1,10 +1,8 @@
 import {
-  BoardConfig,
   BoardPattern,
   GridBoardConfig,
 } from "../lib/abstractBoard/boardFactory";
 import { CoordinateLike } from "../lib/coordinate";
-import { Grid } from "../lib/grid";
 import { getOuterBorder } from "../lib/group_utils";
 import { BadukBoard, BadukConfig, Color } from "./baduk";
 
@@ -12,14 +10,6 @@ export type LegacyBadukConfig = {
   width: number;
   height: number;
   komi: number;
-};
-
-export type BadukState = {
-  board: SerializedBadukBoard<Color>;
-  next_to_play: 0 | 1;
-  captures: { 0: number; 1: number };
-  last_move: string;
-  score_board?: SerializedBadukBoard<Color>;
 };
 
 /** Returns true if the group containing (x, y) has at least one liberty. */
@@ -52,40 +42,4 @@ export function getWidthAndHeight(config: GridBadukConfig): {
   const width = "board" in config ? config.board.width : config.width;
   const height = "board" in config ? config.board.height : config.height;
   return { width: width, height: height };
-}
-
-export declare type SerializedBadukBoard<TColor> = {
-  key: { x: number; y: number };
-  value: TColor;
-}[];
-
-export function serializeBadukBoard<TColor>(
-  fillable: BadukBoard<TColor>,
-): SerializedBadukBoard<TColor> {
-  const result: SerializedBadukBoard<TColor> = [];
-
-  fillable.forEach((value, key) =>
-    result.push({ key: { x: key.x, y: key.y }, value: value }),
-  );
-
-  return result;
-}
-
-export function deserializeBadukBoard<TColor>(
-  serializedBoard: SerializedBadukBoard<TColor>,
-): Grid<TColor> {
-  const { x: height, y: width } = serializedBoard.reduce<{
-    x: number;
-    y: number;
-  }>(
-    (previous, current) => ({
-      x: Math.max(previous.x, current.key.x),
-      y: Math.max(previous.y, current.key.y),
-    }),
-    { x: 0, y: 0 },
-  );
-
-  const result = new Grid<TColor>(width, height);
-  serializedBoard.forEach((entry) => result.set(entry.key, entry.value));
-  return result;
 }

62e0ec978dcbacc4168ccc820fbbfef168a24470
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2024-09-08 17:34:02 -0400

    Allow header fields to be set after construction.

--- packages/shared/src/lib/sgf_recorder.ts
+++ packages/shared/src/lib/sgf_recorder.ts
@@ -2,22 +2,30 @@ import { Color } from "../variants/baduk";
 import { Coordinate } from "./coordinate";
 
 export class SgfRecorder {
-  private sgfContent_ = "";
-  public get sgfContent() {
-    return this.sgfContent_;
+  private body_: string = "";
+  get sgfContent() {
+    return this.makeHeader() + this.body_;
   }
 
   constructor(
-    variant: string,
-    boardSize: { width: number; height: number },
-    komi: number,
-  ) {
-    const { width, height } = boardSize;
-    const szString = width === height ? String(width) : `${width}:${height}`;
-    this.sgfContent_ += `(;\nEV[GO Variants]\nPB[Player Black]\nPW[Player White]\nSZ[${szString}]\nKM[${komi}]\nVS[${variant}]\n\n`;
+    public variant: string = "",
+    public boardSize: { width: number; height: number } = { width: 19, height: 19 },
+    public komi: number = 0,
+  ) {}
+
+  recordMove(move: Coordinate, player: Color) {
+    this.body_ += `${player % 2 === 0 ? ";B" : ";W"}[${move.toSgfRepr()}]`;
   }
 
-  public recordMove(move: Coordinate, player: Color) {
-    this.sgfContent_ += `${player % 2 === 0 ? ";B" : ";W"}[${move.toSgfRepr()}]`;
+  private makeHeader(): string {
+    const { width, height } = this.boardSize;
+    const szString = width === height ? String(width) : `${width}:${height}`;
+    return `(;
+EV[GO Variants]
+PB[Player Black]
+PW[Player White]
+SZ[${szString}]
+KM[${this.komi}]
+VS[${this.variant}]\n\n`;
   }
 }

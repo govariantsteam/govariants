74ef4b6aad4b8430705d06488ba6af85261b8cc3
merowin <martin.unger@planet-earth.at>
2024-10-13 17:52:34 +0200

    first implementation of fractional (part 2)

--- packages/shared/src/variants/s_fractional.ts
+++ packages/shared/src/variants/s_fractional.ts
@@ -1,121 +1,291 @@
-import { Coordinate } from "../lib/coordinate";
-import { Grid } from "../lib/grid";
+import { Coordinate, CoordinateLike } from "../lib/coordinate";
+import {
+  BoardPattern,
+  createBoard,
+  createGraph,
+} from "../lib/abstractBoard/boardFactory";
+import { NewBadukConfig } from "./baduk_utils";
 import { AbstractGame } from "../abstract_game";
-import { BoardConfig } from "../lib/abstractBoard/boardFactory";
-import { Color } from "./fractional/fractional";
+import { BadukBoard } from "./baduk";
+import { Intersection } from "../lib/abstractBoard/intersection";
+import { GraphWrapper } from "../lib/graph";
+import { Grid } from "../lib/grid";
 
-export type SFractionalConfig = {
-  komi: number;
-  board: BoardConfig;
-  secondary_colors: number;
-  players_per_team: number;
-};
+declare type Color = string;
+declare type PlacementColors = [Color, Color] | [];
+declare type StonePlacement = [{ x: number; y: number }, [Color, Color]];
+declare type ScorePlacement = [{ x: number; y: number }, Color];
+
+export type SFractionalConfig = NewBadukConfig & { secondary_colors: Color[] };
 
 export type SFractionalState = {
-  board: Color[][][] | [number, Color[]][];
+  stonePlacements: StonePlacement[];
+  lastMove: string;
+  scorePlacements?: ScorePlacement[];
 };
 
 export class SFractional extends AbstractGame<
   SFractionalConfig,
   SFractionalState
 > {
-  public board: Grid<number | null>;
-  protected next_to_play: 0 | 1 = 0;
+  protected last_move = "";
+  public board: BadukBoard<PlacementColors>;
+  public scorePlacements?: ScorePlacement[];
 
-  // Initialize state of the game for a given config
-  constructor(config?: SFractionalConfig) {
-    // AbstractGame will set this.config to the defaultConfig() if config is undefined.
-    // Therefore, we access config through this.config in the rest of this function
+  constructor(config: SFractionalConfig) {
     super(config);
 
-    if (this.config.width >= 25 || this.config.height >= 25) {
-      // You can throw from this function to signify an invalid config
-      // It is highly recommended to put a cap on board size to prevent
-      // overloading the CPU
-      throw new Error("Board too big!");
-    }
+    if (this.config.board.type === "grid") {
+      if (this.config.board.width >= 52 || this.config.board.height >= 52) {
+        throw new Error("Baduk does not support sizes greater than 52");
+      }
 
-    this.board = new Grid<Color>(this.config.width, this.config.height).fill(
-      null,
-    );
+      this.board = new Grid<PlacementColors>(
+        this.config.board.width,
+        this.config.board.height,
+      ).fill([]);
+    } else {
+      const intersections = createBoard(this.config.board, Intersection);
+      this.board = new GraphWrapper(createGraph(intersections, []));
+    }
   }
 
-  // Return an object that represents the state of the game as it should be displayed to
-  // the user.  The player parameter can be used to determine which parts of the state
-  // should not be shown.
-  override exportState(/* player?: number */): SFractionalState {
+  private isOccupied(colors: PlacementColors): colors is [Color, Color] {
+    return colors.length > 0;
+  }
+
+  override exportState(): SFractionalState {
+    const stonePlacements: StonePlacement[] = [];
+
+    this.board.forEach((placementColors, index) => {
+      if (this.isOccupied(placementColors)) {
+        stonePlacements.push([{ x: index.x, y: index.y }, placementColors]);
+      }
+    });
+
     return {
-      board: this.board.to2DArray(),
+      stonePlacements: stonePlacements,
+      lastMove: this.last_move,
+      ...(this.scorePlacements && { scorePlacements: this.scorePlacements }),
     };
   }
 
-  // Return an array of the players who can place a stone in this round
   override nextToPlay(): number[] {
-    // Return empty array if game is over.
-    // You can check the phase property of AbstractGame
-    if (this.phase === "gameover") {
-      return [];
-    }
-
-    // Return a list of the players who may submit a move
-    // [0, 1] means either player can make a move - you will likely want to change this
-    return [0, 1];
+    return this.phase === "gameover" ? [] : [this.round % 2];
   }
 
-  // Validate move and update the state accordingly
   override playMove(player: number, move: string): void {
-    if (move === "resign") {
-      // Set phase to "gameover" to end the game
-      // phase is a property of the AbstractGame class
+    if (move === "resign" || move === "timeout") {
       this.phase = "gameover";
-      // result is also a property of the AbstractGame class, and is displayed to the user
-      this.result = this.next_to_play === 0 ? "W+R" : "B+R";
+      this.result = player === 0 ? "W+R" : "B+R";
       return;
     }
 
-    if (move === "timeout") {
-      // "timeout" is a special move sent by the system, and is handled similar
-      // "resign"
-      this.phase = "gameover";
-      this.result = player === 0 ? "W+T" : "B+T";
-      return;
+    if (!this.nextToPlay().includes(player)) {
+      throw Error(`It's not player ${player}'s turn.`);
     }
 
-    if (move == "pass") {
-      // It is important to increase the round in order for game playback
-      // and timers to work
-      super.increaseRound();
-      return;
+    if (move != "pass") {
+      const decoded_move = this.decodeMove(move);
+
+      const placementColors = this.board.at(decoded_move);
+      if (placementColors === undefined) {
+        throw Error(
+          `Move out of bounds. (move: ${decoded_move}, config: ${JSON.stringify(this.config)}`,
+        );
+      }
+      if (this.isOccupied(placementColors)) {
+        throw Error(
+          `Cannot place a stone on top of an existing stone. (${placementColors.join(", ")} at (${decoded_move.x}, ${decoded_move.y}))`,
+        );
+      }
+
+      this.playMoveInternal(decoded_move);
     }
-
-    const decoded_move = Coordinate.fromSgfRepr(move);
-    if (!this.board.isInBounds(decoded_move)) {
-      // You can throw from the playMove() function to signify invalid moves
-      throw Error(
-        `Move out of bounds. (move: ${decoded_move}, board dimensions: ${this.config.width}x${this.config.height}`,
-      );
-    }
-
-    // After checking the move is valid
-    this.board.set(decoded_move, player);
-
-    // It is important to increase the round in order for game playback
-    // and timers to work
+    this.prepareForNextMove(move);
     super.increaseRound();
+  }
+
+  private decodeMove(move: string): Coordinate {
+    if (this.config.board.type == "grid") {
+      return Coordinate.fromSgfRepr(move);
+    }
+    // graph boards encode moves with the unique identifier number
+    return new Coordinate(Number(move), 0);
+  }
+
+  private nextStoneColors(): [Color, Color] {
+    const primaryColor = this.round % 2 === 0 ? "black" : "white";
+
+    const pairedRoundsIndex = Math.floor(this.round / 2);
+    const secondaryColor =
+      this.config.secondary_colors[
+        pairedRoundsIndex % this.config.secondary_colors.length
+      ];
+    return [primaryColor, secondaryColor];
+  }
+
+  private playMoveInternal(move: Coordinate): void {
+    const stoneColors = this.nextStoneColors();
+    this.board.set(move, stoneColors);
+
+    // calculate captures
+    var groupsWithoutLiberties: { x: number; y: number }[][] = [];
+
+    this.board.neighbors(move).forEach((pos) => {
+      const neighborStoneColors = this.board.at(pos);
+
+      if (
+        neighborStoneColors === undefined ||
+        !this.isOccupied(neighborStoneColors)
+      ) {
+        return;
+      }
+
+      // check neighbours for captured groups not connected to the placed stone
+      for (const color of neighborStoneColors.filter(
+        (color) => !stoneColors.includes(color),
+      )) {
+        const examResult = this.examineGroup(pos, color);
+        if (examResult.liberties === 0) {
+          groupsWithoutLiberties.push(examResult.members);
+        }
+      }
+
+      // remove neighbouring groups without liberties
+      for (const coord of groupsWithoutLiberties.flat()) {
+        this.board.set(coord, []);
+      }
+      groupsWithoutLiberties = [];
+
+      // check placed stone for self capture, which is allowed
+      for (const color of stoneColors) {
+        const examResult = this.examineGroup(move, color);
+        if (examResult.liberties === 0) {
+          groupsWithoutLiberties.push(examResult.members);
+        }
+      }
+
+      // remove self-capture groups
+      for (const coord of groupsWithoutLiberties.flat()) {
+        this.board.set(coord, []);
+      }
+    });
+  }
+
+  private examineGroup(
+    index: CoordinateLike,
+    color: Color,
+  ): { members: { x: number; y: number }[]; liberties: number } {
+    const members: { x: number; y: number }[] = [];
+    var liberties = 0;
+    const board = this.board;
+    const visited = this.board.map(() => false);
+
+    function searchHelper(index: { x: number; y: number }): void {
+      if (visited.at(index)) {
+        return;
+      }
+
+      const stoneColors = board.at(index);
+
+      if (stoneColors === undefined) {
+        // should not happen
+        return;
+      }
+
+      if (stoneColors.length === 0) {
+        liberties++;
+      } else if (stoneColors.includes(color)) {
+        members.push(index);
+      }
+
+      visited.set(index, true);
+      board.neighbors(index).forEach(searchHelper);
+    }
+
+    searchHelper(index);
+    return { members: members, liberties: liberties };
+  }
+
+  protected prepareForNextMove(move: string): void {
+    if (move == "pass" && this.last_move === "pass") {
+      this.finalizeScore();
+    } else {
+      this.last_move = move;
+    }
+  }
+
+  private finalizeScore(): void {
+    const scorePlacements: ScorePlacement[] = [];
+    const visited = this.board.map((_) => false);
+    const board = this.board;
+
+    // setup vars and functions
+    var potentialOwnerColors: Color[] = ["black", "white"];
+    var territoryContainer: { x: number; y: number }[] = [];
+    function resetVars(): void {
+      potentialOwnerColors = ["black", "white"];
+      territoryContainer = [];
+    }
+    function territoryIdentifier(index: { x: number; y: number }): void {
+      if (visited.at(index) || potentialOwnerColors.length === 0) {
+        return;
+      }
+
+      const stoneColors = board.at(index);
+
+      if (stoneColors === undefined) {
+        // should not happen
+        return;
+      }
+
+      if (stoneColors.length === 0) {
+        territoryContainer.push(index);
+      } else {
+        potentialOwnerColors.filter((color) => stoneColors.includes(color));
+      }
+
+      visited.set(index, true);
+      board.neighbors(index).forEach(territoryIdentifier);
+    }
+
+    // identify territory over the board
+    this.board.forEach((placementColors, index) => {
+      if (this.isOccupied(placementColors)) {
+        return;
+      }
+
+      territoryIdentifier(index);
+      if (potentialOwnerColors.length === 1) {
+        const owner = potentialOwnerColors[0];
+        territoryContainer.forEach((coord) =>
+          scorePlacements.push([coord, owner]),
+        );
+      }
+      resetVars();
+    });
+
+    this.phase = "gameover";
   }
 
   override numPlayers(): number {
     return 2;
   }
 
-  // These show up as buttons in the UI
-  // The key is the string that is passed to playMove(), and the value is
-  // the string that is displayed on the button
   override specialMoves(): { [key: string]: string } {
     return { pass: "Pass", resign: "Resign" };
   }
 
   defaultConfig(): SFractionalConfig {
-    return { width: 19, height: 19 };
+    return {
+      komi: 7.5,
+      board: {
+        type: BoardPattern.Grid,
+        width: 19,
+        height: 19,
+      },
+      secondary_colors: ["blue", "red"],
+    };
   }
 }

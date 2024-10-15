293b05abb5bcad914cfd631e061f061316bb9b9c
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2024-10-14 20:57:35 -0400

    Use markdown instead of html for rules.

--- packages/shared/src/templates/fractional_rules.ts
+++ packages/shared/src/templates/fractional_rules.ts
@@ -1,47 +1,38 @@
 export const fractionalRulesDescription = `
+Introduction
+------------
 
-<h2>Introduction</h2>
-<p>
-  Fractional is a variant of the classical game of Baduk (Go) featuring <strong>Parallel Moves</strong> and <strong>Fractional Stones</strong>. It supports a wide range of game configurations and can be played with any number of players.
-</p>
-<p>
-  Understanding the rules of Baduk is a prerequisite for this rules description.
-</p>
+Fractional is a variant of the classical game of Baduk (Go) featuring **Parallel Moves** and **Fractional Stones**. It supports a wide range of game configurations and can be played with any number of players.
 
-<h2>Objective</h2>
-<p>
-  Players receive points for the area controlled by their <strong>Primary Colour</strong> (see <i>Fractional Stones</i>).
-</p>
+Understanding the rules of Baduk is a prerequisite for this rules description.
 
-<h2>Parallel Moves</h2>
-<p>
-  When all players have submitted a move, all stones are simultaneously placed on the board. If two or more players played the same move, a collision occurs (see <i>Collision Handling</i>).
-</p>
+Objective
+---------
 
-<h2>Fractional Stones</h2>
-<p>
-  Stones have multiple colours, and the chains of each colour are determined individually. As a result chains may overlap with each other. Players place stones with two colours, one of them being the players <strong>Primary Colour</strong>.
-</p>
+Players receive points for the area controlled by their **Primary Colour** (see _Fractional Stones_).
 
-<h2>Collision Handling</h2>
-<p>
-  When two or more players submit the same move, a single stone is placed at that position, with all colours of the colliding players.
-</p>
+Parallel Moves
+--------------
 
-<h2>Capture</h2>
-<p>
-  Chains without liberties are removed as usual. When multiple chains simultaneously lose their last liberty, first all Old Chains are removed simultaneously, then all New Chains without liberties are removed.
-</p>
+When all players have submitted a move, all stones are simultaneously placed on the board. If two or more players played the same move, a collision occurs (see _Collision Handling_).
 
-<ul>
-  <li>
-    <strong>New Chains</strong> are those including a stone placed this round.
-  </li>
-  <li>
-    <strong>Old Chains</strong> are those that don’t include a stone placed this round.
-  </li>
-</ul>
+Fractional Stones
+-----------------
 
-<p>
-  Note that removing Old Chains might free up liberties of New Chains, allowing them to avoid capture (like in Baduk when a stone captures a Ko).
-</p>`;
+Stones have multiple colours, and the chains of each colour are determined individually. As a result chains may overlap with each other. Players place stones with two colours, one of them being the players **Primary Colour**.
+
+Collision Handling
+------------------
+
+When two or more players submit the same move, a single stone is placed at that position, with all colours of the colliding players.
+
+Capture
+-------
+
+Chains without liberties are removed as usual. When multiple chains simultaneously lose their last liberty, first all Old Chains are removed simultaneously, then all New Chains without liberties are removed.
+
+* **New Chains** are those including a stone placed this round.
+* **Old Chains** are those that don't include a stone placed this round.
+
+Note that removing Old Chains might free up liberties of New Chains, allowing them to avoid capture (like in Baduk when a stone captures a Ko).
+`;

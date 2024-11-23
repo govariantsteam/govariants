681c4baf33859cd8185e581a882d3ed45b6048db
merowin <martin.unger@planet-earth.at>
2024-11-23 19:49:50 +0100

    add padding on mobile, limit text size in rules page

--- packages/vue-client/src/views/RulesView.vue
+++ packages/vue-client/src/views/RulesView.vue
@@ -17,7 +17,7 @@ const variantExists = computed(() => getVariantList().includes(props.variant));
 <template>
   <main>
     <div v-if="!variantExists">Sorry, variant not found.</div>
-    <div v-if="variantExists" class="grid-page-layout large-grid">
+    <div v-if="variantExists" class="grid-page-layout">
       <div>
         <h1>{{ toUpperCaseFirstLetter(props.variant) }} Variant Rules</h1>
         <p v-if="!rulesDescription">Under Construction</p>
@@ -39,6 +39,7 @@ main {
 .grid-page-layout {
   max-width: none;
   overflow-x: scroll;
+  grid-template-columns: minmax(45ch, 90ch) 1fr;
 }
 
 :deep(strong, h1, h2) {

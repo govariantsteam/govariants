10b9d1ab1c49e7bcf344ca5697208ee082849359
merowin <martin.unger@planet-earth.at>
2024-12-05 21:06:01 +0100

    add tetris rules description

---
+++ packages/shared/src/templates/tetris_rules.ts
@@ -0,0 +1,8 @@
+export const tetrisRules = `
+Introduction
+------------
+
+Tetris Go is a variant of Go / Baduk where in addition to the normal rules, moves that create a chain with a Tetris shape are not allowed.
+In other words, players can't create chains of size 4 (and this is how Tetris Go works on non rectangular boards).
+Note that players can still create chains of size 5 or more by connecting two chains.
+`;

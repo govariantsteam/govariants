3d07e4398456a8d55ef395a1be60a725512dd5f9
merowin <77507448+merowin@users.noreply.github.com>
2024-12-19 19:44:10 +0100

    Revert "Revert "Lighthouse (renamed)""

--- packages/vue-client/src/components/boards/MulticolorGridBoard.vue
+++ packages/vue-client/src/components/boards/MulticolorGridBoard.vue
@@ -15,6 +15,7 @@ const props = defineProps<{
   background_color?: string;
   board_dimensions: { width: number; height: number };
   score_board?: (string[] | null)[][];
+  fields_bg_color?: (string | null)[][];
 }>();
 
 const width = computed(() => props.board_dimensions.width);
@@ -51,6 +52,19 @@ function positionHovered(pos: Coordinate) {
       :height="height"
       :fill="background_color ?? '#dcb35c'"
     />
+    <g v-if="props.fields_bg_color">
+      <rect
+        v-for="pos in positions.filter((pos) =>
+          props.fields_bg_color?.at(pos.y)?.at(pos.x),
+        )"
+        :key="`${pos.x},${pos.y}`"
+        :x="pos.x - 0.5"
+        :y="pos.y - 0.5"
+        width="1"
+        height="1"
+        :fill="props.fields_bg_color![pos.y][pos.x]!"
+      ></rect>
+    </g>
     <g>
       <line
         v-for="x in width"

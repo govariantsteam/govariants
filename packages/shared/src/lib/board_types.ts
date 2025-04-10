056658c04de8bdc93395928188e77441a69b8eee
merowin <martin.unger@planet-earth.at>
2025-04-10 12:42:22 +0200

    add comment

--- packages/shared/src/lib/board_types.ts
+++ packages/shared/src/lib/board_types.ts
@@ -3,6 +3,8 @@ import { BoardConfig } from "./abstractBoard/boardFactory";
 export interface MulticolorStone {
   colors: string[];
   annotation?: "CR" | "MA";
+
+  // currently only supported on grid board
   background_color?: string;
   disable_move?: true;
 }

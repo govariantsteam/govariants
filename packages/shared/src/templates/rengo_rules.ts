a1570285cce6eb825ed597bfac886c01e4a50dea
merowin <martin.unger@planet-earth.at>
2025-09-27 20:09:34 +0200

    improve rules description

--- packages/shared/src/templates/rengo_rules.ts
+++ packages/shared/src/templates/rengo_rules.ts
@@ -1,7 +1,7 @@
 export const rengoRulesDescription = `
-Rengo is a team variant of Baduk / Go. Members of a team alternate in playing moves for their color.<br>
+Rengo is a team variant of Baduk / Go. Members of a team alternate playing moves for their color.<br>
 <br>
-Here we generalised Rengo to work as a modification on top of another arbitrary (sub)variant. Every player position of the subvariant corresponds to a team of multiple players. Members of a team play moves for the corresponding player position in the subgame, and do so in a cyclic alternating order.<br>
+Here we generalised Rengo to work as a modification on top of another arbitrary (sub)variant. Every player position of the subvariant corresponds to a team of multiple players in rengo. Members of a team play moves for the corresponding player position in the subgame, and do so in a cyclic alternating order.<br>
 <br>
 More information on original rengo: [https://senseis.xmp.net/?Rengo](https://senseis.xmp.net/?Rengo)
 `;

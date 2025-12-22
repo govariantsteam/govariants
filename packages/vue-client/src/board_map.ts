f1a5cf06605f2bfd64a4e2f151bf26797d950211
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2025-12-21 19:59:31 -0500

    Revert changes in board_map

--- packages/vue-client/src/board_map.ts
+++ packages/vue-client/src/board_map.ts
@@ -2,21 +2,18 @@ import ParallelGoBoard from "@/components/boards/ParallelGoBoard.vue";
 import ChessBoard from "@/components/boards/ChessBoard.vue";
 import FractionalBoard from "@/components/boards/FractionalBoard.vue";
 import KeimaBoard from "@/components/boards/KeimaBoard.vue";
-import CubeBoard from "@/components/boards/CubeBoard.vue";
 import type { Component } from "vue";
 import QuantumBoard from "@/components/boards/QuantumBoard.vue";
 import DefaultBoard from "./components/boards/DefaultBoard.vue";
 import PolymorphicBoard from "./components/boards/PolymorphicBoard.vue";
 
 const board_map: {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [variant: string]: Component<any>;
+  [variant: string]: Component<{ config: unknown; gamestate: unknown }>;
 } = {
   parallel: ParallelGoBoard,
   chess: ChessBoard,
   fractional: FractionalBoard,
   keima: KeimaBoard,
-  cube: CubeBoard,
   quantum: QuantumBoard,
   rengo: PolymorphicBoard,
 };

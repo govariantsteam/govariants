eebeb1ac443586028e36c569e0c5e26d7110d687
merowin <martin.unger@planet-earth.at>
2026-01-19 12:37:27 +0100

    info text

--- packages/vue-client/src/views/NotificationsView.vue
+++ packages/vue-client/src/views/NotificationsView.vue
@@ -14,13 +14,14 @@ import {
   faCircleCheck,
   faTrash,
   faGear,
+  faBell,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import GameListItem from "@/components/GameListItem.vue";
 import GameListItemFallback from "@/components/GameListItemFallback.vue";
 import SubscriptionDialog from "@/components/GameView/SubscriptionDialog.vue";
 
-library.add(faCircleCheck, faTrash, faGear);
+library.add(faCircleCheck, faTrash, faGear, faBell);
 
 const notificationGroups: Ref<NotificationsResponse[] | null> = ref(null);
 const store = useStore();
@@ -85,69 +86,76 @@ async function clear(gameId: string): Promise<unknown> {
 </script>
 
 <template>
-  <main>
-    <div>
-      <div
-        class="game-notifications-container"
-        v-for="{ gameId, notifications, gameState } in notificationGroups"
-        :key="gameId"
-      >
-        <div class="board-container">
-          <template v-if="isErrorResult(gameState)">
-            <GameListItemFallback
-              :error="gameState.errorMessage"
-              :variant="gameState.variant"
-              :game-id="gameState.id"
-            />
-          </template>
-          <template v-else>
-            <GameListItem :game="gameState" />
-          </template>
-          <div v-for="(notification, index) in notifications" :key="index">
-            <strong v-if="!notification.read" aria-label="unread">
-              {{ renderNotification(notification) }}
-            </strong>
-            <span v-else aria-label="read">
-              {{ renderNotification(notification) }}
-            </span>
-          </div>
-        </div>
-        <div class="actions-container">
-          <button
-            class="icon-button success-color"
-            aria-label="mark as read"
-            v-on:click="markAsRead(gameId)"
-          >
-            <FontAwesomeIcon icon="fa-solid fa-circle-check" />
-          </button>
-          <button
-            class="icon-button alert-color"
-            aria-label="clear"
-            v-on:click="clear(gameId)"
-          >
-            <FontAwesomeIcon icon="fa-solid fa-trash" />
-          </button>
-          <button
-            class="icon-button grey-color"
-            :disabled="!user"
-            v-on:click="isDialogOpen = true"
-          >
-            <FontAwesomeIcon icon="fa-solid fa-gear" />
-          </button>
-          <SubscriptionDialog
-            v-if="!isErrorResult(gameState)"
-            :gameId="gameId"
-            :subscription="gameState.subscription ?? []"
-            :is-open="isDialogOpen"
-            v-on:close="isDialogOpen = false"
-          ></SubscriptionDialog>
+  <div class="notifications-page">
+    <div
+      class="game-notifications-container"
+      v-for="{ gameId, notifications, gameState } in notificationGroups"
+      :key="gameId"
+    >
+      <div class="board-container">
+        <template v-if="isErrorResult(gameState)">
+          <GameListItemFallback
+            :error="gameState.errorMessage"
+            :variant="gameState.variant"
+            :game-id="gameState.id"
+          />
+        </template>
+        <template v-else>
+          <GameListItem :game="gameState" />
+        </template>
+        <div v-for="(notification, index) in notifications" :key="index">
+          <strong v-if="!notification.read" aria-label="unread">
+            {{ renderNotification(notification) }}
+          </strong>
+          <span v-else aria-label="read">
+            {{ renderNotification(notification) }}
+          </span>
         </div>
       </div>
+      <div class="actions-container">
+        <button
+          class="icon-button success-color"
+          aria-label="mark as read"
+          v-on:click="markAsRead(gameId)"
+        >
+          <FontAwesomeIcon icon="fa-solid fa-circle-check" />
+        </button>
+        <button
+          class="icon-button alert-color"
+          aria-label="clear"
+          v-on:click="clear(gameId)"
+        >
+          <FontAwesomeIcon icon="fa-solid fa-trash" />
+        </button>
+        <button
+          class="icon-button grey-color"
+          :disabled="!user"
+          v-on:click="isDialogOpen = true"
+        >
+          <FontAwesomeIcon icon="fa-solid fa-gear" />
+        </button>
+        <SubscriptionDialog
+          v-if="!isErrorResult(gameState)"
+          :gameId="gameId"
+          :subscription="gameState.subscription ?? []"
+          :is-open="isDialogOpen"
+          v-on:close="isDialogOpen = false"
+        ></SubscriptionDialog>
+      </div>
     </div>
-  </main>
+    <p v-if="user === null">Please log in to view your users notifications.</p>
+    <p v-if="notificationGroups?.length === 0">
+      You have no notifications currently. Subscribe to a games notifications by
+      clicking on
+      <FontAwesomeIcon icon="fa-solid fa-bell" /> .
+    </p>
+  </div>
 </template>
 
 <style scoped>
+.notifications-page {
+  padding: 2em;
+}
 .game-notifications-container {
   margin: 20px;
   padding: 5px;
